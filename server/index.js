23b59703707cd6f8ace64c4a8142f2f1d6f23e99
Rafael Segarra <rafaelandres54@gmail.com>
2021-02-02 16:05:58 -0500

    update homepage

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
 module.exports = app
+const cors = require('cors')
 
 // This is a global Mocha hook, used for resource cleanup.
 // Otherwise, Mocha v4+ never quits after tests.
@@ -43,6 +44,8 @@ passport.deserializeUser(async (id, done) => {
 const createApp = () => {
   // logging middleware
   app.use(morgan('dev'))
+
+  app.use(cors())
 
   // body parsing middleware
   app.use(express.json())
