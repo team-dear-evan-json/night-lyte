bcb68d64b20744b818c227583fa1b5a010022601
Kama580 <kleetal@gmail.com>
2021-02-02 16:19:18 -0500

    chore: marker design

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, MainMap} from './components'
+import {Login, Signup, UserHome, MainMap, MainMapDummy} from './components'
 import {me} from './store'
 
 /**
@@ -18,7 +18,8 @@ class Routes extends Component {
 
     return (
       <Switch>
-        <Route parth="/home" component={MainMap} />
+        <Route path="/home" component={MainMap} />
+        <Route path="/test" component={MainMapDummy} />
         {/* Routes placed here are available to all visitors */}
         {/* <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
